2e8271e1a370f937e11313d5307f30769a0e9758
zhaosg <zhaosg89@163.com>
2017-09-01 09:46:02 +0800

    commit

--- src/ios/vfr/SDVReaderMainToolbar.m
+++ src/ios/vfr/SDVReaderMainToolbar.m
@@ -126,9 +126,11 @@
 
         UIButton *doneButton = [UIButton buttonWithType:UIButtonTypeCustom];
         doneButton.frame = CGRectMake(leftButtonX, BUTTON_Y, doneButtonWidth, BUTTON_HEIGHT);
-        [doneButton setTitleColor:[UIColor colorWithWhite:0.0f alpha:1.0f] forState:UIControlStateNormal];
-        [doneButton setTitleColor:[UIColor colorWithWhite:1.0f alpha:1.0f] forState:UIControlStateHighlighted];
-        [doneButton setTitle:doneButtonText forState:UIControlStateNormal];
+        [doneButton setImage:[UIImage imageNamed:@"SDVReader-back"] forState:UIControlStateNormal];
+
+//        [doneButton setTitleColor:[UIColor colorWithWhite:0.0f alpha:1.0f] forState:UIControlStateNormal];
+//        [doneButton setTitleColor:[UIColor colorWithWhite:1.0f alpha:1.0f] forState:UIControlStateHighlighted];
+//        [doneButton setTitle:doneButtonText forState:UIControlStateNormal];
         doneButton.titleLabel.font = doneButtonFont;
         [doneButton addTarget:self action:@selector(doneButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
         [doneButton setBackgroundImage:buttonH forState:UIControlStateHighlighted];
